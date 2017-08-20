a4fe93492edf9413317393e62eca39a3c0ffd48c
GatoL <17721327159@163.com>
2017-08-20 22:06:42 +1000

    use pyautogui to get (x,y) of mouse
    use Tkinter to get resolution of screen, use Image of PIL to process pic.

---
+++ MouseMagic/mouseMagic1.py
@@ -0,0 +1,33 @@
+# -*- coding:UTF-8 -*-
+import pyautogui as pag
+import Tkinter as tk
+from PIL import Image
+import random
+root=tk.Tk()
+screen_width=root.winfo_screenwidth()
+screen_height=root.winfo_screenheight()
+window=Image.new("RGB",(screen_width,screen_height),(0,0,0))
+print screen_height,screen_width
+r=int(random.uniform(0,255))
+g=int(random.uniform(0,255))
+b=int(random.uniform(0,255))
+print r,g,b
+try:
+    while True:
+            r+=1
+            g+=1
+            b+=1
+            if r==255:
+                r=0
+            if g==255:
+                g=0
+            if b==255:
+                b=0
+            time.sleep(0.01)
+            x,y = pag.position() #返回鼠标的坐标
+            if window.getpixel((x,y))[0]==0:
+                window.putpixel((x,y),(r,g,b))
+except  KeyboardInterrupt:
+    window.resize((500,800))
+    window.save('window.png','PNG')
+    print 'save successfully....'
