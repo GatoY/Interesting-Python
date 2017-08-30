3873d0d73ec4f2664ed0e2eb1b432d0108e886c6
GatoY <derakhy@gmail.com>
2017-08-30 23:12:33 +1000

    insertionsort

--- sortAlgorithm/sort.py
+++ sortAlgorithm/sort.py
@@ -1,1 +1,16 @@
+# -*- coding:utf-8 -*-
+import time
+#insertions sort. Worst case running time is n^2, linear for the almost sorted array.
+def insertionsort(s):
+    startinsert=time.clock()
+    for i in range(1,len(s)):
+        a=s[i]
+        j=i-1
+	while (j>=0 and a<s[j]):
+	    s[j+1]=s[j]
+	    j=j-1
+	s[j+1]=a
+    endinsert=time.clock()
+    print '花的时间为 '+str(endinsert-startinsert)
+    return s
  
